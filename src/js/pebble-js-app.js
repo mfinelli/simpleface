0c92002cf0eb3e63d3c7e8b32f30515c10bb3c98
Mario Finelli <mtfinelli@gmail.com>
2015-12-26 09:41:40 -0500

    Fetch weather data from forecast.io

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,7 +1,68 @@
+const API_KEY = ''
+
+var xhrRequest = function(url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  var url = 'https://api.forecast.io/forecast/' + API_KEY + '/' +
+    pos.coords.latitude + ',' + pos.coords.longitude;
+
+  // Send request to forecast.io.
+  xhrRequest(url, 'GET',
+      function(responseText) {
+        // responseText contains a JSON object with weather info.
+        var json = JSON.parse(responseText);
+
+        var temperature = Math.round(json.currently.temperature);
+        console.log('Temperature is: ' + temperature);
+
+        var conditions = json.currently.summary;
+        console.log('Conditions are: ' + conditions);
+
+        // Assemble dictionary using our keys
+        var dictionary = {
+          'KEY_TEMPERATURE': temperature,
+          'KEY_CONDITIONS': conditions
+        };
+
+        // Send to pebble.
+        Pebble.sendAppMessage(dictionary,
+            function(e) {
+              console.log('Weather info sent to pebble successfully!');
+            },
+            function(e) {
+              console.log('Error sending weather info to pebble!');
+            }
+        );
+      }
+  );
+}
+
+function locationError(err) {
+  console.log('Error requesting location!');
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+      locationSuccess,
+      locationError,
+      { timeout: 15000, maximumAge: 60000 }
+  );
+}
+
 // Listen for when the watchface is opened.
 Pebble.addEventListener('ready',
     function(e) {
       console.log('PebbleKit JS ready!');
+
+      // Get the initial weather
+      getWeather();
     }
 );
 
@@ -9,5 +70,6 @@ Pebble.addEventListener('ready',
 Pebble.addEventListener('appmessage',
     function(e) {
       console.log('AppMessage received');
+      getWeather();
     }
 );
--- src/simpleface.c
+++ src/simpleface.c
@@ -62,6 +62,27 @@ static void main_window_unload(Window *window) {
 
 static void inbox_received_callback(DictionaryIterator *iterator,
         void *context) {
+    // Store incoming information.
+    static char temperature_buffer[8];
+    static char conditions_buffer[32];
+    static char weather_layer_buffer[32];
+
+    // Read tuples for data.
+    Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+    Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+    // Iff all data is available then use it.
+    if(temp_tuple && conditions_tuple) {
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%d°F",
+                (int)temp_tuple->value->int32);
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s",
+                conditions_tuple->value->cstring);
+
+        // Assemble full string and display.
+        snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s %s",
+                temperature_buffer, conditions_buffer);
+        text_layer_set_text(s_weather_layer, weather_layer_buffer);
+    }
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
